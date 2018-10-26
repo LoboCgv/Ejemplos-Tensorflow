ff681cc684411f88ef1a66b1d5f33a01a54543de
LoboCgv <42422293+LoboCgv@users.noreply.github.com>
2018-10-26 15:47:20 -0300

    OpenCV leer camara de PC

---
+++ VerCamara.py
@@ -0,0 +1,12 @@
+from cv2 import *
+vc = VideoCapture(0);
+while True:
+    next, frame = vc.read()
+
+  #  gray = cvtColor(frame, COLOR_BGR2GRAY)
+  #  gauss = GaussianBlur(gray, (7,7), 1.5, 1.5)
+  #  can = Canny(gauss, 0, 30, 3)
+
+    imshow("webcam", frame)
+    if waitKey(50) >= 0:
+        break;
